2d09853effadbef82c9e396a1033ef97eaecf2c7
Justin Ramirez <jusrez528@gmail.com>
2022-07-11 18:53:32 -0400

    Complete user story 5

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/toolboxes/:id', to: 'toolboxes#show'
   get '/tools', to: 'tools#index'
   get '/tools/:id', to: 'tools#show'
-  
+  get '/toolboxes/:id/tools', to: 'toolbox_tools#index'
 end
