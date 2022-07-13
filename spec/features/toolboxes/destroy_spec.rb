f1e37d5c742b87df2032f8d8fa420a61e8776726
Justin Ramirez <jusrez528@gmail.com>
2022-07-13 15:52:49 -0400

    Complete user story 22

--- spec/features/toolboxes/destroy_spec.rb
+++ spec/features/toolboxes/destroy_spec.rb
@@ -5,11 +5,23 @@ RSpec.describe 'Toolbox Delete' do
     hammers = Toolbox.create!(name: "Hammers", tool_capacity: 5, checked_out: false)
     hand_hammer = Tool.create!(name: "Hand Hammer", working_condition: false, quantity: 2, toolbox_id: hammers.id)
     
-    visit("toolboxes/#{hammers.id}")
+    visit "toolboxes/#{hammers.id}"
 
     click_link "Delete Toolbox"
 
     expect(current_path).to eq("/toolboxes")
+    expect(page).to_not have_content("Hammers")
+  end
+
+  it 'when i visit the toolbox index page i will see a link to delete the parent' do
+    hammers = Toolbox.create!(name: "Hammers", tool_capacity: 5, checked_out: false)
+    hand_hammer = Tool.create!(name: "Hand Hammer", working_condition: false, quantity: 2, toolbox_id: hammers.id)
+
+    visit '/toolboxes'
+
+    click_link "Delete Toolbox"
+
+    expect(current_path).to eq('/toolboxes')
     expect(page).to_not have_content("Hammers")
   end
 end
